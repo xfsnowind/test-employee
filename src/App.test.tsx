95fc0c91fb4608ba6ce9d03b2cfee8efe4566449
xfsnowind <xfsnowind@gmail.com>
2025-09-03 00:17:09 +0800

    feat: add the command to check typescript
    update the test file and ts config

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
 import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
 import App from "./App";
 
 test("renders Employee Form heading", () => {
   render(<App />);
-  expect(
-    screen.getByRole("heading", { name: /employee form/i })
-  ).toBeInTheDocument();
+  const heading = screen.getByRole("heading", { name: /employee form/i });
+  if (!heading) throw new Error("heading not found");
 });
